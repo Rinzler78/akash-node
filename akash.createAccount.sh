40c1fe6342dbf480ddf9956ba780cca9aba094f5
Boris LECLERE <Borisleclere@gmail.com>
2021-08-18 17:34:02 +0200

    Remove commands in createAccount script

--- akash.createAccount.sh
+++ akash.createAccount.sh
@@ -1,6 +1,1 @@
-echo "akash key name : $AKASH_KEY_NAME"
 akash keys add $AKASH_KEY_NAME
-
-export AKASH_ACCOUNT_ADDRESS="$(akash keys show $AKASH_KEY_NAME -a)"
-
-echo "akash account adress : $AKASH_ACCOUNT_ADDRESS"
