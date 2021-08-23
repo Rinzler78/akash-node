a8efabc39fe766a1ba0469575531a25ea2eff64e
Boris LECLERE <Borisleclere@gmail.com>
2021-08-23 08:47:17 +0200

    Add akash status

---
+++ akash.status.sh
@@ -0,0 +1,14 @@
+function trace {
+    echo "********************** $1 *****************************"
+}
+
+trace "akash status"
+echo "AKASH_DSEQ => $AKASH_DSEQ"
+echo "AKASH_OSEQ => $AKASH_OSEQ"
+echo "AKASH_GSEQ => $AKASH_GSEQ"
+echo "AKASH_KEY_NAME => $AKASH_KEY_NAME"
+echo "AKASH_NODE => $AKASH_NODE"
+echo "AKASH_ACCOUNT_ADDRESS => $AKASH_ACCOUNT_ADDRESS"
+echo "AKASH_MONIKER => $AKASH_MONIKER"
+echo "AKASH_CHAIN_ID => $AKASH_CHAIN_ID"
+trace "akash status"
