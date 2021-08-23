e474cc9d7d3cd8491ba90a5e25369b2a4c0221ed
Boris LECLERE <Borisleclere@gmail.com>
2021-08-23 15:40:08 +0200

    state

--- akash.fastSync.state.set.sh
+++ akash.fastSync.state.set.sh
@@ -12,15 +12,13 @@ NEW_STATE=$(echo $NEW_STATE | tr '[:upper:]' '[:lower:]')
 #echo "Arg => $1"
 #echo "NEW_STATE => $NEW_STATE"
 
-NEW_STATE="fast_sync = $NEW_STATE"
-
 case $NEW_STATE in
     true | false)
-        NEW_STATE="version = $NEW_STATE"
+        NEW_STATE="fast_sync = $NEW_STATE"
         ./helper.file.replace.sh "$HOME/.akash/config/config.toml" "$CURRENT_STATE" "$NEW_STATE"
         exit
     
     ;;
 esac
 
-echo "Error : Bad arguments. Need \"v0\" or \"v2\""
+echo "Error : Bad arguments. Need \"true\" or \"false\""
