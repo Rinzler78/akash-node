1fae8f80d7fcdb29fe89a92a4556cdb11606ff2b
Boris LECLERE <Borisleclere@gmail.com>
2021-08-24 11:11:01 +0200

    Try

--- akash.account.create.sh
+++ akash.account.create.sh
@@ -1,7 +1,6 @@
 if [ -z $AKASH_KEY_NAME ];then
-    echo "Warning : AKASH_KEY_NAME not set. Pleas enter a name"
-    export AKASH_KEY_NAME=$(read line)
+    echo "Warning : AKASH_KEY_NAME not set. Please enter a name"
+    AKASH_KEY_NAME=$(read line)
 fi
-
 
 akash keys add $AKASH_KEY_NAME
