04f9d6d2054560f2abf8a9279aa1b385620b5aa9
Boris LECLERE <Borisleclere@gmail.com>
2021-08-24 11:56:54 +0200

    Menu add status and run full node

--- menu.sh
+++ menu.sh
@@ -1,6 +1,6 @@
-title="Akash menu"
+title="Akash control menu"
 prompt="Select an option :"
-options=("Update akash " "Set key name (AKASK_KEY_NAME)" "Create account" "Get account address")
+options=("Update akash " "Set key name (AKASK_KEY_NAME)" "Create account" "Get account address" "ENV status" "Run full node")
 
 echo "$title"
 PS3="$prompt "
@@ -18,6 +18,30 @@ select opt in "${options[@]}" "Quit"; do
     ;;
     4) echo "You picked $opt "
         ./akash.account.address.sh
+    ;;
+    5) echo "You picked $opt "
+        ./akash.status.sh
+    ;;
+    6) echo "You picked $opt "
+        export AKASH_MONIKER="TestWalletMoniker"
+        echo "AKASH_MONIKER => $AKASH_MONIKER"
+        export AKASH_CHAIN_ID="$(./akash.chainId.get.sh)"
+        echo "AKASH_CHAIN_ID => $AKASH_CHAIN_ID"
+        ./akash.node.Init.sh
+
+        export GAS_PRICE=0.025
+        ./akash.gasPrice.sh
+        ./akash.gasPrice.set.sh $GAS_PRICE
+
+        ./akash.genesisFile.download.sh
+        ./akash.genesisFile.validate.sh
+
+        ./akash.fastSync.state.set.enable.sh
+        ./akash.fastSync.version.set.v2.sh
+        
+        ./akash.seeds.set.default.sh
+        
+        ./akash.node.start.sh
     ;;
     $((${#options[@]}+1))) echo "Goodbye!"; break;;
     *) echo "Invalid option. Try another one.";continue;;
