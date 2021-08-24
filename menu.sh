f05f3e1f28d7635ee719ff184397abdb02a5634f
Boris LECLERE <Borisleclere@gmail.com>
2021-08-24 12:26:11 +0200

    Improv menu management

--- menu.sh
+++ menu.sh
@@ -1,48 +1,68 @@
 title="Akash control menu"
 prompt="Select an option :"
-options=("Update akash " "Set key name (AKASK_KEY_NAME)" "Create account" "Get account address" "ENV status" "Run full node")
+options=("Update akash" "Set key name (AKASK_KEY_NAME)" "Create account" "Get account address" "ENV status" "Run full node")
 
 echo "$title"
 PS3="$prompt "
 
-function executeCommadn 
+function executeCommand 
 {
-    case "$REPLY" in
-    1) echo "You picked $opt"
+    command=$1
+    commandName=${options[$command]}
+
+    echo "Executing command [$command] => $commandName"
+
+    case "$command" in
+    1) # Update
         ./update.sh
     ;;
-    2) echo "You picked $opt"
+    2) # Create account
         ./akash.account.create.sh
     ;;
-    3) echo "You picked $opt "
+    3) # Set AKASH_KEY_NAME
         read AKASK_KEY_NAME
         echo "AKASK_KEY_NAME => $AKASK_KEY_NAME"
     ;;
-    4) echo "You picked $opt "
+    4) # Get account Address
         ./akash.account.address.sh
     ;;
-    5) echo "You picked $opt "
+    5) # akash env status
         ./akash.status.sh
     ;;
-    6) echo "You picked $opt "
+    6) # Node init
         export AKASH_MONIKER="TestWalletMoniker"
         echo "AKASH_MONIKER => $AKASH_MONIKER"
         export AKASH_CHAIN_ID="$(./akash.chainId.get.sh)"
         echo "AKASH_CHAIN_ID => $AKASH_CHAIN_ID"
         ./akash.node.Init.sh
-
+    ;;
+    7) # Set Gas Price
+        ./akash.gasPrice.sh
         export GAS_PRICE=0.025
         ./akash.gasPrice.set.sh $GAS_PRICE
         ./akash.gasPrice.sh
-
+    ;;
+    8) # Init genesis file
         ./akash.genesisFile.download.sh
         ./akash.genesisFile.validate.sh
-
+    ;;
+    9) # Enable fast sync
+        ./akash.fastSync.state.sh
         ./akash.fastSync.state.set.enable.sh
         ./akash.fastSync.version.set.v2.sh
-        
+        ./akash.fastSync.state.sh
+    ;;
+    10) # Set seeds
+        ./akash.seeds.sh
         ./akash.seeds.set.default.sh
-        
+        ./akash.seeds.sh
+    ;;
+    11) # Full Node Start
+        executeCommand 6
+        executeCommand 7
+        executeCommand 8
+        executeCommand 9
+        executeCommand 10
         ./akash.node.start.sh
     ;;
     esac
@@ -50,54 +70,10 @@ function executeCommadn
 
 select opt in "${options[@]}" "Quit"; do 
     case "$REPLY" in
-    [1-${#options[@]}]) echo "You picked $opt"
-        executeCommadn $REPLY
+    [1-${#options[@]}])
+        executeCommand $REPLY
     ;;
     $((${#options[@]}+1))) echo "Goodbye!"; break;;
     *) echo "Invalid option. Try another one.";continue;;
     esac
 done
-
-# select opt in "${options[@]}" "Quit"; do 
-#     case "$REPLY" in
-#     1) echo "You picked $opt"
-#         ./update.sh
-#     ;;
-#     2) echo "You picked $opt"
-#         ./akash.account.create.sh
-#     ;;
-#     3) echo "You picked $opt "
-#         read AKASK_KEY_NAME
-#         echo "AKASK_KEY_NAME => $AKASK_KEY_NAME"
-#     ;;
-#     4) echo "You picked $opt "
-#         ./akash.account.address.sh
-#     ;;
-#     5) echo "You picked $opt "
-#         ./akash.status.sh
-#     ;;
-#     6) echo "You picked $opt "
-#         export AKASH_MONIKER="TestWalletMoniker"
-#         echo "AKASH_MONIKER => $AKASH_MONIKER"
-#         export AKASH_CHAIN_ID="$(./akash.chainId.get.sh)"
-#         echo "AKASH_CHAIN_ID => $AKASH_CHAIN_ID"
-#         ./akash.node.Init.sh
-
-#         export GAS_PRICE=0.025
-#         ./akash.gasPrice.set.sh $GAS_PRICE
-#         ./akash.gasPrice.sh
-
-#         ./akash.genesisFile.download.sh
-#         ./akash.genesisFile.validate.sh
-
-#         ./akash.fastSync.state.set.enable.sh
-#         ./akash.fastSync.version.set.v2.sh
-        
-#         ./akash.seeds.set.default.sh
-        
-#         ./akash.node.start.sh
-#     ;;
-#     $((${#options[@]}+1))) echo "Goodbye!"; break;;
-#     *) echo "Invalid option. Try another one.";continue;;
-#     esac
-# done
