e09fa04fa8a5237be6348210189fc56a8cf84967
Boris LECLERE <Borisleclere@gmail.com>
2021-08-24 11:15:32 +0200

    Debug

--- akash.account.create.sh
+++ akash.account.create.sh
@@ -1,6 +1,6 @@
 if [ -z $AKASH_KEY_NAME ];then
     echo "Warning : AKASH_KEY_NAME not set. Please enter a name"
-    AKASH_KEY_NAME=$(read line)
+    read AKASH_KEY_NAME
 fi
 
 echo "akash : Create account with $AKASH_KEY_NAME"
